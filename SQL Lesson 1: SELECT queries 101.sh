a49b73ba5e0bfa26ca60f141e1eb408e2340fa65
aperez14 <35715550+aperez14@users.noreply.github.com>
2023-12-17 19:57:43 -0800

    Create SQL Lesson 1: SELECT queries 101.sh

---
+++ SQL Lesson 1: SELECT queries 101.sh
@@ -0,0 +1,13 @@
+Select query for a specific columns
+SELECT column, another_column, …
+FROM mytable;
+
+Select query for all columns
+SELECT * 
+FROM mytable;
+
+SELECT title FROM movies;
+SELECT director FROM movies; 
+SELECT title, director FROM movies; 
+SELECT title, year FROM movies; 
+SELECT * FROM movies; 
